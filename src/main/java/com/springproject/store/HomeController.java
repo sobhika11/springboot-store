fe0f64f95e44e94bea31d599c30716954bd68a0c
Sobhika <sobhika1105@gmail.com>
2026-06-10 23:50:46 +0530

    build success

--- src/main/java/com/springproject/store/HomeController.java
+++ src/main/java/com/springproject/store/HomeController.java
@@ -1,6 +1,9 @@
 package com.springproject.store;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HomeController {
@@ -9,4 +12,9 @@ public class HomeController {
     public String index(){
         return "index.html";
     }
+    @RequestMapping("path", method=RequestMethod.GET)
+    public String requestMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
 }
--- src/main/java/com/springproject/store/StoreApplication.java
+++ src/main/java/com/springproject/store/StoreApplication.java
@@ -1,5 +1,4 @@
 package com.springproject.store;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
