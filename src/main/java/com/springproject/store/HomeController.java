c62d7e3c94acc6888718edb3017dcbcb064a3392
Sobhika <sobhika1105@gmail.com>
2026-06-16 14:34:12 +0530

    Rest API

--- src/main/java/com/springproject/store/HomeController.java
+++ src/main/java/com/springproject/store/HomeController.java
@@ -1,20 +1,22 @@
 package com.springproject.store;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
-
+    ArrayList<String>products=new ArrayList<>(Arrays.asList("Iphone","Camera","Smart watch"));
     @RequestMapping("/")
     public String index(){
         return "index.html";
     }
-    @RequestMapping("path", method=RequestMethod.GET)
-    public String requestMethodName(@RequestParam String param) {
-        return new String();
+    @GetMapping("/products")
+    public List<String> pro(){
+        return products;
     }
     
 }
